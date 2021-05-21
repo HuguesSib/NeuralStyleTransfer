a9ca751157786472485049e736db9cead1b94eb2
Javier <javas@kth.se>
2021-05-21 15:48:31 +0200

    final testing

--- testJavi.py
+++ testJavi.py
@@ -67,11 +67,11 @@ def style_content_loss(style_outputs, content_outputs, style_targets, content_ta
     content_weight=1e4
 
     style_loss = tf.add_n([tf.reduce_mean((style_outputs[i]-gram_matrix(style_targets[i]))**2) 
-                           for i in len(style_outputs)])
+                           for i in range(len(style_outputs))])
     style_loss *= style_weight / num_style_layers
 
     content_loss = tf.add_n([tf.reduce_mean((content_outputs[i]-content_targets[i])**2) 
-                             for i in len(content_outputs)])
+                             for i in range(len(content_outputs))])
     content_loss *= content_weight / num_content_layers
     loss = style_loss + content_loss
     return loss
